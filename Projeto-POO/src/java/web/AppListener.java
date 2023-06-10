1eb2e785d3e11f782bbb9d810a8d3d658d0de042
MMeneses1 <134298798+MMeneses1@users.noreply.github.com>
2023-06-10 15:31:30 -0300

    Atualizações em AppListener.java

--- Projeto-POO/src/java/web/AppListener.java
+++ Projeto-POO/src/java/web/AppListener.java
@@ -8,54 +8,62 @@ import java.sql.*;
 import java.security.*;
 import java.math.*;
 import model.User;
-//import model.VehicleStay;
 
 @WebListener
 public class AppListener implements ServletContextListener {
+    // Constantes para a conexão com o banco de dados
     public static final String CLASS_NAME = "org.sqlite.JDBC";
     public static final String URL = "jdbc:sqlite:parkapp.db";
+    
+    // Variáveis para armazenar informações de inicialização e exceções
     public static String initializeLog = "";
     public static Exception exception = null;
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
+        // Método vazio para tratar o evento de destruição do contexto da aplicação
     }
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         try {
+            // Estabelecendo a conexão com o banco de dados
             Connection c = AppListener.getConnection();
             Statement s = c.createStatement();
+            
+            // Registrando a data e hora da criação do banco de dados
             initializeLog += new Date() + ": Initializing database creation; ";
-            //USERS
+            
+            // Criando a tabela "Users" se ela não existir
             initializeLog += "Creating Users table if not exists...";
             s.execute(User.getCreateStatement());
             initializeLog += "done; ";
+            
+            // Verificando se não há usuários no banco de dados
             if(User.getUsers().isEmpty()){
                 initializeLog += "Adding default users...";
+                
+                // Inserindo usuários padrão na tabela "Users"
                 User.insertUser("admin", "Administrador", "ADMIN", "1234");
                 initializeLog += "Admin added; ";
-                User.insertUser("fulano", "Fulano da Silva", "USER", "1234");
+                User.insertUser("Rodrigo", "Rodrigo Cruz", "USER", "1234");
                 initializeLog += "Fulano added; ";
             }
-            //VEHICLE
-            initializeLog += "Creating VehicleStay table if not exists...";
-            //s.execute(VehicleStay.getCreateStatement());
-            initializeLog += "done.";
-            s.close();
-            c.close();
+            
         } catch (Exception ex) {
+            // Registrando uma exceção, se ocorrer
             initializeLog += "Erro: " + ex.getMessage();
         }
     }
 
+    // Método para gerar um hash MD5 a partir de uma string
     public static String getMd5Hash(String text) throws NoSuchAlgorithmException {
         MessageDigest m = MessageDigest.getInstance("MD5");
         m.update(text.getBytes(), 0, text.length());
         return new BigInteger(1, m.digest()).toString();
     }
 
+    // Método para obter a conexão com o banco de dados
     public static Connection getConnection() throws Exception {
         Class.forName(CLASS_NAME);
         return DriverManager.getConnection(URL);
